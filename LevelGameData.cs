d2701022a9f8ac0a812e7e7d94ecd20d2ce41b75
AvKov <inknowgt@gmail.com>
2023-12-25 01:58:12 +0700

    Level Data Added

---
+++ LevelGameData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelGameData", menuName = "ScriptableObjects/LevelData", order = 1)]
+public class LevelGameData : ScriptableObject
+{
+    public int levelNumber;
+    public Levels[] levels;
+}
