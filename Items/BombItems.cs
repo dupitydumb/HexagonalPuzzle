5a235781a111bcb579330da218b2f93c47e7ba19
AvKov <37872714+dupitydumb@users.noreply.github.com>
2024-02-01 20:34:33 +0700

    Move Bomb

--- Items/BombItems.cs
+++ Items/BombItems.cs
@@ -13,6 +13,7 @@ public class BombItems : BoosterItems
     void Start()
     {
         base.GetLimits();
+        grid = GameObject.FindWithTag("Grid").GetComponent<Grid>();
         gridData = GridData.Instance;
         
     }
@@ -20,7 +21,10 @@ public class BombItems : BoosterItems
     // Update is called once per frame
     void Update()
     {
-        
+        if(CheckBelow())
+        {
+            MoveDown();
+        }
     }
 
     /// <summary>
@@ -77,34 +81,102 @@ public class BombItems : BoosterItems
 
         foreach (Vector2 pos in neighborPos)
         {
-            int gridIndex = GridData.Instance.gridContainers.FindIndex(element => element.x == pos.x && element.y == pos.y);
-
-            if (gridData.gridContainers[gridIndex].gameObject.tag == "HexagonBlock")
+            int gridIndex = GridData.Instance.gridContainers.FindIndex(element => element.x == pos.x && element.y == pos.y );
+            if (gridIndex != -1)
             {
-                gridData.gridContainers[gridIndex].gameObject.GetComponent<HexagonBlock>().DestroyHexagonBlock();
+                if (gridData.gridContainers[gridIndex].gameObject.tag == "HexagonBlock")
+                {
+                    gridData.gridContainers[gridIndex].gameObject.GetComponent<HexagonBlock>().DestroyHexagonBlock();
+                }
+
+                if (gridData.gridContainers[gridIndex].gameObject.tag == "BoxItems")
+                {
+                    gridData.gridContainers[gridIndex].gameObject.GetComponent<Box>().DestroyObstacle();
+                }    
             }
-
-            if (gridData.gridContainers[gridIndex].gameObject.tag == "BoxItems")
-            {
-                gridData.gridContainers[gridIndex].gameObject.GetComponent<Box>().DestroyObstacle();
-            }    
-           
-            
             
         }
 
+        neighborPoss = neighborPos;
         DestroyBomb();
     }
 
-
+    public List<Vector2> neighborPoss = new List<Vector2>();
     public void DestroyBomb()
     {
+        GridGenerator.Instance.isBombing = true;
         LeanTween.scale(gameObject, new Vector3(3, 3, 0), 0.5f).setOnComplete(() => {
             int gridIndex = GridData.Instance.gridContainers.FindIndex(element => element.x == xPos && element.y == yPos);
             gridData.gridContainers[gridIndex].gameObject = GridGenerator.Instance.guideGrid;
             GridGenerator.Instance.isBombing = false;
+            
             Destroy(gameObject);
         });
+        
+    }
+    Vector3Int cellPos = new Vector3Int();
+    bool isMoving = false;
+    private void MoveDown()
+    {
+        isMoving = true;
+        GridGenerator.Instance.isMoving = true;
+        Vector3 cellCenterPos = grid.GetCellCenterWorld(cellPos);
+        transform.position = Vector3.MoveTowards(transform.position, cellCenterPos, speed * Time.deltaTime);
+
+        //Invoke GridGenerator onGridChanges event
+        GridGenerator.Instance.onGridChanges.Invoke();
+        
+        if (transform.position == cellCenterPos)
+        {
+            isMoving = false;
+            GridGenerator.Instance.isMoving = false;
+            //Find Next position index from list
+            int nextIndex = GridData.Instance.gridContainers.FindIndex(element => element.x == cellPos.x && element.y == cellPos.y);
+            GridData.Instance.gridContainers[nextIndex].gameObject = this.gameObject;
+            //Remove previous position index from list
+            int previousIndex = GridData.Instance.gridContainers.FindIndex(element => element.x == xPos && element.y == yPos);
+            GridData.Instance.gridContainers[previousIndex].gameObject = GridGenerator.Instance.guideGrid;
+            //Set new position
+            xPos = cellPos.x;
+            yPos = cellPos.y;
+
+            //Remove occupied
+            GridData.Instance.gridContainers.Find(element => element.x == xPos && element.y == yPos).isOccupied = null;
+            
+        }
+        
+    }
+    bool isDestroying = false;
+    public bool CheckBelow()
+    {
+
+        if (isDestroying == false )
+        {
+            bool isChecking = false;
+            //PointTop Hexagon
+            cellPos = new Vector3Int(xPos - 1, yPos, 0);
+
+            if (isChecking == false)
+            {
+                if (GridData.Instance == null || GridData.Instance.gridContainers == null)
+                {
+                    return false;
+                }
+                if (GridData.Instance.gridContainers.Exists(element => element.x == cellPos.x && element.y == cellPos.y && element.gameObject.tag == "GuideGrid" && element.gameObject.tag != "HexagonBlock" && element.gameObject != null))
+                {
+                    GridData.Instance.gridContainers.Find(element => element.x == cellPos.x && element.y == cellPos.y).isOccupied = this.gameObject;
+                    return true;
+                }
+                if (GridData.Instance.gridContainers.Exists(element => element.x == cellPos.x && element.y == cellPos.y && element.isOccupied == this.gameObject))
+                {
+                    return true;
+                
+                }
+            }
+            
+        }
+        return false;
+        
         
     }
 }
